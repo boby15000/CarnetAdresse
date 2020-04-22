f8e0448ee92c9a5670c30f6dccb390544b0a901c
Nicolas <boby15000@hotmail.com>
2020-04-22 19:42:32 +0200

    Mise à jour de la partie "Contact"

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,85 +3,43 @@
 namespace App\Controller;
 
 use App\Entity\Contact;
-use App\Entity\DemandeByMail;
+use App\Entity\Invitation;
 use App\Form\ContactType;
-use App\Form\DemandeByMailType;
+use App\Form\InvitationType;
 use App\Repository\ContactRepository;
 use App\Service\Email\MailJet;
-use App\Service\Invitation;
 use App\Service\Email\Message;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
 
 
 class ContactController extends AbstractController
 {
 
-    
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    private $em;  // Pour enregistrer/modifier les données. ObjectManager $em
-    private $repository; // Pour récuperer les données.
-    private $keyPublic; // Clef publique permetant de récuperer les données uniquement lié à cette clef.
-
-    public function __construct(ContactRepository $repository )
-    {
-        $this->em = "";
-        $this->repository = $repository;
-        $this->keyPublic = 0;
-    }
-
-
-	/**
+    /**
      * Affiche les contacts en base de données.
+     * 
      * @Route("/Contact/Show", name="Contact.Show")
      * @return Symfony\Component\HttpFoundation\Response;
      */
-    public function Show(PaginatorInterface $paginator, Request $request)
+    public function Show(ContactRepository $contactRepository, PaginatorInterface $paginator, Request $request)
     {
-
+        // Recuperation de la Clef privé.
         $keyPrivate = $this->getUser()->getKeyPrivate();
 
-        $indexLetter = "A";
-        if ( $request->query->get('letter') != null )
-        { $indexLetter = $request->query->get('letter'); }
+        // Détermine le lettre dont les contacts commenceront.
+        $indexLetter = ( $request->query->get('letter') === null ) ? 'A' : $request->query->get('letter');
+        $indexLetter = ( $request->query->get('letter') === null && $request->getSession()->get('letter') !== null ) ? $request->getSession()->get('letter') : $indexLetter ;
 
-        $lesContacts = $paginator->paginate(
-            $this->repository->findAllByKeyAndLetter($indexLetter, $keyPrivate),
-            $request->query->getInt('page',1),5);
+        // Selectionne les contacts en fonction de la lettre et du paginator.
+        $lesContacts = $paginator->paginate($contactRepository->findAllByKeyAndLetter($indexLetter, $keyPrivate),$request->query->getInt('page',1),5);
 
+        // Parametre le paginator.
         $lesContacts->setCustomParameters([
             'align' => 'center', # center|right (for template: twitter_bootstrap_v4_pagination)
             'size' => 'small', # small|large (for template: twitter_bootstrap_v4_pagination)
@@ -89,77 +47,99 @@ class ContactController extends AbstractController
             'span_class' => 'whatever',
         ]);
 
-        // Défini si un contact à été modifié.
-        $modifSuccess = $request->query->get('modifSuccess');
-        // Défini si un contact à été supprimé.
-        $DeleteSuccess = $request->query->get('DeleteSuccess');
-        // Défini si une invitation à été envoyé.
-        $mailEnvoye = $request->query->get('mailEnvoye');
+        // Enregistrement de l'index lettre
+        $request->getSession()->set('letter', $indexLetter);
+     
+        // Retourne la vue des contacts.
+        return $this->render('Contact/Show.html.twig', ['lesContacts' => $lesContacts,'IndexLetter' => $indexLetter]);
 
-        
-
-        return $this->render('Contact/Show.html.twig', [
-            'lesContacts' => $lesContacts,'IndexLetter' => $indexLetter, 'modifSuccess' => $modifSuccess, 'DeleteSuccess' => $DeleteSuccess, 'mailEnvoye' => $mailEnvoye
-        ]);
     }
+
+
+
+/**********************************************************************************************************/
+
 
 
     /**
      * Ajouter un contact en base de données.
+     * 
      * @Route("/Contact/Add", name="Contact.Add")
      * @return Symfony\Component\HttpFoundation\Response;
      */
     public function Add(Request $request)
     {
-        $result = false; // Variable indiquant si un ajout en base de donnée à été fait.
         $contact = new contact(); // initiale un nouveau contact.
         $form = $this->createForm(ContactType::class, $contact);
         $form->handleRequest($request); // Enregistre le valeur de POST dans un objet contact.
         // Controle si le formulaire est émis.
         if ($form->isSubmitted() && $form->isValid())
         {
-            $em = $this->GetDoctrine()->GetManager(); // Attribut un "ObjectManager".
+            // Recuperation de la Clef privé.
+            $keyPrivate = $this->getUser()->getKeyPrivate();
+            $contact->setPrivateKey($keyPrivate);
+
+            // Enregistre le contact en base de données.
+            $em = $this->GetDoctrine()->GetManager(); 
             $em->persist($contact);
             $em->flush();
-
-            $result = true;
+           
             $contact = new contact(); // initiale un nouveau contact.
+            $form = $this->createForm(ContactType::class, $contact); // Défini un formulaire vierge
+
+            // Message flash de success.
+            $this->addFlash('contact','Votre contact est enregistré.');
+
         }
 
         // Retourne la page d'ajout de contact. La variable "result" permet de définir si un contact vient d'être ajouté.
-        return $this->render('Contact/Add.html.twig',
-            ['form' => $form->createView(), 'result' => $result]);
+        return $this->render('Contact/Add.html.twig', ['form' => $form->createView()]);
     }
+
+
+
+
+/**********************************************************************************************************/
+
 
 
     /**
      * Modifier un contact en base de données.
+     * 
      * @Route("/Contact/Edit-{id}", name="Contact.Edit", requirements={"id"="\d+"})
      * @return Symfony\Component\HttpFoundation\Response;
      */
     public function Edit(Contact $contact, Request $request)
     {
-        $form = $this->createForm(ContactType::class, $contact);
+        $form = $this->createForm(ContactType::class, $contact); // Défini un formulaire avec les données
         $form->handleRequest($request);
 
         // Controle si le formulaire est émis.
         if ($form->isSubmitted() && $form->isValid())
         {
+            // Enregistre les modifications en base de données.
             $em = $this->getDoctrine()->getManager();
             $em->flush();
-            $modifSuccess = true;
+            
+            // Message flash de success.
+            $this->addFlash('contact','Votre contact '. $contact->getNomComplet() .' est modifié.');
 
-            return $this->redirectToRoute('Contact.Show', ['modifSuccess' => $modifSuccess]);
+            return $this->redirectToRoute('Contact.Show');
         }
 
-        return $this->render('Contact/Edit.html.twig',
-            ['form' => $form->createView()]);
+        return $this->render('Contact/Edit.html.twig', ['form' => $form->createView()]);
 
     }
 
 
+
+/**********************************************************************************************************/
+
+
+
     /**
      * Supprime un contact en base de données.
+     * 
      * @Route("/Contact/Delete-{id}", name="Contact.Delete", requirements={"id"="\d+"})
      * @return Symfony\Component\HttpFoundation\Response;
      */
@@ -168,42 +148,56 @@ class ContactController extends AbstractController
         $em = $this->getDoctrine()->getManager();
         $em->remove($contact);
         $em->flush();
-        $DeleteSuccess = true;
 
-        return $this->redirectToRoute('Contact.Show', ['DeleteSuccess' => $DeleteSuccess]);
+        // Message flash de success.
+        $this->addFlash('contact','Votre contact '. $contact->getNomComplet() .' est supprimé.');
+
+        return $this->redirectToRoute('Contact.Show');
 
     }
+
+
+
+/**********************************************************************************************************/
 
 
     /**
      * Demande d'adresse par mail.
-     * @Route("/Contact/Demande", name="Contact.Demande")
+     * @Route("/Contact/Invitation", name="Contact.Demande")
      * @return Symfony\Component\HttpFoundation\Response;
      */
-    public function InvitationByMail(Request $request, Invitation $invitation, MailJet $MailJet)
+    public function InvitationDemande(Request $request)
     {
-        $MessagePerso = $request->request->get('message');
-        $mailEnvoye = false; // Variable indiquant si une invitation est faite.
-        $demande = new DemandeByMail(); // Initialise une nouvelle Demande.
-        $form = $this->createForm(DemandeByMailType::class, $demande, ['nomComplet' => $this->getUser()->getNomComplet() ]); // Initialise le formulaire de Demande.
-        $form->handleRequest($request); // Enregistre les valeur de POST dans l'objet 'nouvelle Demande'.
+       
+        $messageEmail = $this->getUser()->getNomComplet() . " à besoin de vous pour remplir vos informations suivantes : Adresse postale, email, téléphone.";
+        $invitation = new Invitation(); // Génére une nouvelle invitation
+        $form = $this->createForm(InvitationType::class, $invitation, ['messageEmail'=> $messageEmail]); // Défini un formulaire avec les données
+        $form->handleRequest($request);
 
         // Controle si le formulaire est émis.
         if ($form->isSubmitted() && $form->isValid())
         {
-            // Génére l'invitation.
-            $invitation->GetInvitation($this->getUser(), $demande, $request->request->get('message'));
-            // Variable indiquant si une invitation est faite.
-            $mailEnvoye = true;
-            // Redirige l'utilisateur sur la liste des contacts
-            return $this->redirectToRoute('Contact.Show', ['mailEnvoye' => $mailEnvoye]);
-
+        
+            // Enregistre l'invitation
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($invitation);
+            
+            dump($invitation->getArrayDestinataires());
+            // Envoie le mail d'invitation
+            
+            //$em->flush();
+            // Message flash de success.
+            $this->addFlash('contact','Votre invitation est envoyée.');
         }
-       
-        return $this->render('Contact/Demande.html.twig',
-            ['form' => $form->createView(), 'MessagePerso' => $MessagePerso]);
+
+        return $this->render('Contact/InvitationDemande.html.twig', ['form' => $form->createView(), 'message' => $messageEmail]);
 
     }
+
+
+
+/**********************************************************************************************************/
+
 
 
     /**
@@ -211,9 +205,9 @@ class ContactController extends AbstractController
      * @Route("/Invitation-{key}", name="Contact.Invitation")
      * @return Symfony\Component\HttpFoundation\Response;
      */
-    public function Invitation(Request $request, Invitation $invitation, MailJet $MailJet)
+    public function Invitation(Request $request, Invitation $invitation)
     {
-         return $this->render('Contact/Demande.html.twig',
+        return $this->render('Contact/Demande.html.twig',
             ['form' => $form->createView(), 'MessagePerso' => $MessagePerso]); 
     }
 
